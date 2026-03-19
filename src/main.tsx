dd4a0eeb28cb85fb104cb24b31e45a7c478732ba
Puri Virakarin <pr.virakarin.acc@gmail.com>
2026-03-19 19:58:11 +0800

    feat(ui): configure theme provider, clean up css, and remove old assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme.js'
 import './index.css'
-import App from './App.jsx'
+import App from './App.js'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
